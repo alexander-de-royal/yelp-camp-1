00214fd9d5ce13b9b75c5b96c412787cafb67eab
Alexander Royal <alexander.de.royal@gmail.com>
2025-12-30 11:48:03 -0600

    nesting tutorial completed

--- hello.js
+++ hello.js
@@ -53,3 +53,20 @@
 // {
 //     console.log("You are a director")
 // }
+
+// Password must be 6+ characters
+// Password cannot include space
+const password = prompt("please enter a new password")
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1 ){
+        console.log("Valid Password")
+    } else {
+        console.log("Remove space")
+    }
+    console.log("Long enough password")
+} else {
+    console.log("Password too short")
+}
+
+
