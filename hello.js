3739fb3e5647e56459e8f71ed9b71d943927e862
Alexander Royal <alexander.de.royal@gmail.com>
2025-12-30 11:27:42 -0600

    else if tutorial completed

--- hello.js
+++ hello.js
@@ -1,1 +1,41 @@
-console.log("hello");
+// console.log("Before Conditional")
+// let random = Math.random();
+// if (random <0.5 ){
+//     console.log("Your number is less than 0.5")
+//     console.log(random)
+// }
+// if (random >= 0.5){
+//     console.log("Your number is greater than 0.5")
+//     console.log(random)
+// }
+
+// const dayOfWeek = 'Monday';
+//
+// if (dayOfWeek === 'Monday')
+// {
+//     console.log("I hate mondays")
+// }
+// else if (dayOfWeek === 'Saturday')
+// {
+//     console.log("I love Saturday")
+// }
+
+// const age = 8;
+//
+// if (age < 5)
+// {
+//     console.log("Free")
+// }
+// else if (age < 10)
+// {
+//     console.log("$10")
+// }
+// else if (age < 65)
+// {
+//     console.log("$20")
+// }
+// else
+// {
+//     console.log("$10")
+// }
+
