eabc145b99bdbe4ff8b58da77e78d808ede7c24a
Alexander Royal <alexander.de.royal@gmail.com>
2025-12-31 08:13:06 -0600

    switch tutorial completed

--- hello.js
+++ hello.js
@@ -99,4 +99,22 @@
 //     console.log("$20")
 // }
 
+const day = 2;
 
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+}
