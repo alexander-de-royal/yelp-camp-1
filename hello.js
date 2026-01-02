b65755b4f05ac12822a2279573eebd8f3b62038d
Alexander Royal <alexander.de.royal@gmail.com>
2026-01-02 11:12:10 -0600

    object literals tutorial completed

--- hello.js
+++ hello.js
@@ -99,22 +99,28 @@
 //     console.log("$20")
 // }
 
-const day = 2;
+// const day = 2;
+//
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     default:
+//         console.log("Wee!day!")
+// }
 
-switch (day) {
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thursday");
-        break;
-    case 5:
-        console.log("Friday");
-        break;
-}
+// let movieLine = ['tom', 'nancy']
+// movieLine[2] = 'pablo'
+
