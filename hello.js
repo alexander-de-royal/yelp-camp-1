b9e5067cea8ee14dac3194183370500678b6ca8b
Alexander Royal <alexander.de.royal@gmail.com>
2026-01-04 09:18:39 -0600

    todo list tutorial partially completed

--- hello.js
+++ hello.js
@@ -280,3 +280,24 @@
 //     total += score;
 // }
 // console.log("Average is:", total/scores.length)
+
+let input = prompt("What would you like to do?");
+const todos = ['Collect Chicken Eggs', 'Clean Cat Litter Box'];
+while(input !== 'quit' && input !== 'q'){
+    if (input === 'list'){
+        console.log('*')
+        for (let i = 0; i < todos.length; i++){
+            console.log(`${i}: ${todos[i]}`)
+        }
+        console.log('*')
+    } else if (input === 'new'){
+        const newTodo = prompt('Ok, What is the new todo?');
+        todos.push(newTodo)
+        console.log(`${newTodo} added to the list`)
+    } else if (input === 'delete'){
+        const index = prompt('Okay, what enter an index to delete:')
+        console.log(index)
+    }
+    input = prompt("What would you like to do?")
+}
+console.log("quit the app")
